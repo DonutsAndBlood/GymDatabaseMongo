780965efd949c115840f032f21e182a7658f4f2f
Donuts and Blood <59175438+DonutsAndBlood@users.noreply.github.com>
2022-12-01 10:45:26 -0300

    Ajustes

---
+++ Inserts/Inserts_Alunos.js
@@ -0,0 +1,7 @@
+db.alunos.insertMany([
+{Cpf: '954.206.637-67', Nome_Aluno: 'Pedro Henrique Bruno Danilo da Rosa', Pagamento: 'A', Vencimento_Mensalidade: 28, Alunos_Exercicios: 1.0, Telefone: 27984317939},
+{Cpf: '271.716.287-95', Nome_Aluno: 'Ben?cio Bryan Sales', Pagamento: 'A', Vencimento_Mensalidade: 25, Alunos_Exercicios: 3.0, Telefone: 27984536300},
+{Cpf: '754.213.007-22', Nome_Aluno: 'Arthur Lu?s Silva', Pagamento: 'A', Vencimento_Mensalidade: 27, Alunos_Exercicios: 1.0, Telefone: 27983320915},
+{Cpf: '277.716.817-27', Nome_Aluno: 'Francisco Kaique Victor Silveira', Pagamento: 'A', Vencimento_Mensalidade: 30, Alunos_Exercicios: 2.0, Telefone: 27992422949},
+{Cpf: '678.856.887-12', Nome_Aluno: 'Ant?nia Patr?cia Juliana Drumond', Pagamento: 'I', Vencimento_Mensalidade: 30, Alunos_Exercicios: 2.0, Telefone: 28999628749},
+{Cpf: '015.744.507-00', Nome_Aluno: 'Jorge Tom?s Noah Santos', Pagamento: 'I', Vencimento_Mensalidade: 8, Alunos_Exercicios: 1.0, Telefone: 27982094019}])
